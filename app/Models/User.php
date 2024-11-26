4445cbc7368ad9e5b9c09520323c2ed98377e6c0
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-11-26 19:20:59 +0100

    Paso 2. Relación entre user y post

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -63,5 +65,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function post() : HasMany
+    {
+        return $this->hasMany(Post::class, 'user_id','id');
     }
 }
