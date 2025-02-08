d5421d0d28be78cc364e0e467c290790b9f81418
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2025-02-08 20:25:40 +0100

    Paso 21. Sidebar

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <!--archivos de JS y css-->
-    @vite('resources/css/app.css', 'resources/js/app.js')
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!--archivos de JS y css-->
+        @vite('resources/css/app.css', 'resources/js/app.js')
+    </head>
     <body>
         @include('partials.navbar')
-        <header class="bg-gray-200 dark:bg-gray-800 p-4">
-            {{ $header ?? '' }}
-        </header>
 
-        <main>
-            {{ $slot }}
-        </main>
+        <div class="flex min-h-screen">
+            @include('components.sidebar')
 
+            <div class="flex-1">
+                <header class="bg-gray-200 dark:bg-gray-800 p-4 ml-64">
+                    {{ $header ?? '' }}
+                </header>
+
+                <main class="p-6 ml-64">
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
     </body>
 </html>
